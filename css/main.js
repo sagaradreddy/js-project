59483e45d055a9a72096493f62c4fead77d575ae
sagaradreddy <sagarad671@gmail.com>
2025-07-25 22:52:22 +0530

    time display

---
+++ css/main.js
@@ -0,0 +1,12 @@
+// function blockThread(params) {
+//   let block = Date.now()
+//     for (;Date.now() - block <params;) {
+      
+//     }
+// }
+// blockThread(5000)
+// console.log('unblockNow')
+
+const [,,,a, ...rest] = [10,20,30,40,50]
+console.log(rest)
+
